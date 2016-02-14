1a78fd29ff04329fe2bdcd0080b978a0223b801d
Tom <tgmaxx@gmail.com>
2016-02-14 11:44:07 -0500

    Code updated

--- Arduino IDE Code/MQTT_Alarm_ESP07.ino
+++ Arduino IDE Code/MQTT_Alarm_ESP07.ino
@@ -1,45 +1,29 @@
 //----------------------------------------------------------------------------------
 // NetworX Security System Monitoring with ESP8266
 // Tested on ESP-07 and NodeMCU v1.0
-// February 11, 2016
+// February 13, 2016
 //
 // by tgmaxx  <tgmaxx [at] gmail [dot] com>
-//
-// Licensed under the MIT license, http://opensource.org/licenses/mit-license.php
 //----------------------------------------------------------------------------------
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define wifi_ssid "your router ssid"
-#define wifi_password "your router password"
-#define mqtt_server "your mqtt server ip address"
-#define mqtt_topic "NX8V2"
 #define ledPin 5
 #define aux1Pin 12
 #define aux2Pin 13
 #define aux3Pin 14
 #define aux4Pin 4
 
+const char* wifi_ssid = "your router ssid";
+const char* wifi_password = "your router password";
+const char* mqtt_server = "your mqtt server ip address";
+const char* mqtt_topic = "NX8V2";
+
 WiFiClient espClient;
-PubSubClient client(mqtt_server,1883,callback,espClient);
+PubSubClient client(espClient);
 
 char msgBuff[10];
-
-void setup() {
-    Serial.begin(115200);
-    setup_wifi();
-    pinMode(ledPin,OUTPUT);
-    digitalWrite(ledPin, HIGH);  // Turn the LED off
-    pinMode(aux1Pin,INPUT_PULLUP);
-    pinMode(aux2Pin,INPUT_PULLUP);
-    pinMode(aux3Pin,INPUT_PULLUP);
-    pinMode(aux4Pin,INPUT_PULLUP);
-    attachInterrupt(aux1Pin, onAux1, FALLING);
-    attachInterrupt(aux2Pin, onAux2, FALLING);
-    attachInterrupt(aux3Pin, onAux3, FALLING);
-    attachInterrupt(aux4Pin, onAux4, FALLING);
-}
 
 void setup_wifi() {
   delay(10);
@@ -48,8 +32,8 @@ void setup_wifi() {
   Serial.println(wifi_ssid);
   WiFi.begin(wifi_ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
     delay(500);
+    Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -67,7 +51,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-  for(int i=0; i<length; i++) {
+  for(i=0; i<length; i++) {
     msgBuff[i] = payload[i];
   }
   msgBuff[i] = '\0';
@@ -113,6 +97,23 @@ void onAux3() {
 void onAux4() {
   client.publish(mqtt_topic, "AUX4"); // System Disarmed
   delay(500);
+}
+
+void setup() {
+    pinMode(ledPin,OUTPUT);
+    digitalWrite(ledPin, HIGH);  // Turn the LED off
+    pinMode(aux1Pin,INPUT_PULLUP);
+    pinMode(aux2Pin,INPUT_PULLUP);
+    pinMode(aux3Pin,INPUT_PULLUP);
+    pinMode(aux4Pin,INPUT_PULLUP);
+    attachInterrupt(aux1Pin, onAux1, FALLING);
+    attachInterrupt(aux2Pin, onAux2, FALLING);
+    attachInterrupt(aux3Pin, onAux3, FALLING);
+    attachInterrupt(aux4Pin, onAux4, FALLING);
+    Serial.begin(57600);
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 }
 
 void loop() {
